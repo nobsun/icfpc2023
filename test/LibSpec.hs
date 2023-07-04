e5e04f7c5edaf1e44ecf2bc8d1f59f0c465af38e
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-04 18:14:12 +0900

    not to use unicode-show things

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -4,23 +4,10 @@ module LibSpec
   ) where
 
 import Data.String
-import qualified Codec.Binary.UTF8.String as U
 import qualified Data.ByteString as B
 import Test.Main
 import Test.Hspec
-import Text.Show.Unicode
 import Lib
-
-newtype UString a = UString a deriving Eq
-
-ustring :: B.ByteString -> UString String
-ustring = UString . U.decode . B.unpack
-
-instance IsString a => IsString (UString a) where
-  fromString = UString . fromString
-  
-instance Show a => Show (UString a) where
-  show (UString s) = ushow s
 
 spec :: Spec
 spec = describe "someFunc" $ do
@@ -28,6 +15,6 @@ spec = describe "someFunc" $ do
     { result <- captureProcessResult Lib.someFunc
     ; prExitCode result `shouldBe` ExitSuccess
     ; prStderr result `shouldSatisfy` B.null
-    ; ustring (prStdout result) `shouldBe` "なんか関数\n"
+    ; prStdout result `shouldBe` "some func\n"
     }
   }
