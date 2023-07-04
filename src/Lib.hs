3472763885016bf3bd2dfa5494ee2e2f4ee5367b
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-04 18:14:12 +0900

    not to use unicode-show things

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 -- # 雛形モジュール
 -- このファイルは`stack new`コマンドで自動的に`src/`に挿入されます
--- 
+--
 -- ## 言語拡張と`module`宣言
 -- 最低限の指定をしてある
 {- |
@@ -19,13 +19,10 @@ module Lib
     ( someFunc
     ) where
 
--- ## `doctest`のための記述と定義本体
--- テストは失敗するように書いてある
-
-{- | 
-「なんか関数」を標準出力に印字する
+{- |
+print "some func" to stdout
 >>> someFunc
-なんか関数
+some func
 -}
 someFunc :: IO ()
-someFunc = putStrLn "なんか函数"
+someFunc = putStrLn "some func"
