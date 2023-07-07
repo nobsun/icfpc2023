8bf20dd703800328f322bdc6e718fa425dce0ad5
cutsea110 <cutsea110@gmail.com>
2023-07-07 21:42:50 +0900

    add download script and lightning problems 1-45 by using it

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROBLEMS_DIR=problems
+
+for i in `seq 1 45`
+do
+    curl -H "Accept: application/json" \
+	 -H "Content-type: application/json" \
+	 -X GET https://api.icfpcontest.com/problem?problem_id=${i} \
+	| jq -r '.Success' | jq -c . > ${PROBLEMS_DIR}/${i}.json
+done
