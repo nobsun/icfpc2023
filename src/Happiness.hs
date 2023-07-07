cf6bf7cf41c415a82e56d67c3482686cce0da81e
cutsea110 <cutsea110@gmail.com>
2023-07-08 01:22:27 +0900

    check whether block musician is between musician and attendee

--- src/Happiness.hs
+++ src/Happiness.hs
@@ -31,16 +31,24 @@ happiness prob ans = score
 --   blocker  (bx, by)
 isBlock :: Placement -> Attendee -> Placement -> Bool
 isBlock (Placement mx my) (Attendee ax ay _) (Placement bx by)
-  = distance (line (mx, my) (ax, ay)) (bx, by) <= 5.0
+  = distance (a, b, c) (bx, by) <= 5.0 && between (mx, my) (p, q) (ax, ay)
   where
+    -- mx, my と ax, ay を通る直線の方程式
+    -- a * x + b * y + c = 0
+    -- この a, b, c を求める
+    (a, b, c) = line (mx, my) (ax, ay)
+    -- (x1, y1) (x2, y2) を通る直線へ (x0, y0) からおろした垂線の交点を (p, q) とする
+    (p, q) = (a * k + bx, b * k + by)
+      where
+        k = (a * bx + b * by + c) / (a * a + b * b)
+    
     -- (x1, y1) (x2, y2) を通る直線の方程式
     -- 傾き a = (y2 - y1) / (x2 - x1) として
     -- y = a * (x - x1) + y1
-    line (x1, y1) (x2, y2) = (a, b, c)
-      where
-        a = y2 - y1
-        b = x1 - x2
-        c = x2 * y1 - x1 * y2
+    line (x1, y1) (x2, y2) = (y2 - y1, x1 - x2, x2 * y1 - x1 * y2)
     -- また直線 a * x + b * y + c = 0 と 点 (x0, y0) の距離 d
     distance (a, b, c) (x0, y0) = abs (a * x0 + b * y0 + c) / sqrt (a * a + b * b)
 
+    -- (x0, y0) が (x1, y1) (x2, y2) の間にあるかどうか
+    between (x1, y1) (x0, y0) (x2, y2)
+      = (x0 - x1) * (x0 - x2) <= 0 && (y0 - y1) * (y0 - y2) <= 0
