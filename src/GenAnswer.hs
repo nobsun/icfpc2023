7ea37c917723d9c4392b3d3a81d0a122b5dcd5a3
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-08 04:53:20 +0900

    calculate happiness of solution

--- src/GenAnswer.hs
+++ src/GenAnswer.hs
@@ -2,9 +2,11 @@
 module GenAnswer where
 
 import qualified Data.ByteString.Lazy as B
-import Data.Aeson (encode)
+import Data.Aeson (encode, decode)
 import Text.Printf (printf)
 
+import Answer
+import Problem
 import Solver
 import Happiness
 
@@ -16,3 +18,18 @@ saveAnswer (name, solver) probNum = do
   let path = printf "solutions/%s_%03d.json" name probNum
   putStrLn $ "writing " ++ path --- ++ " : happiness: " ++ show (happiness problem ans)
   B.writeFile path $ encode ans
+
+readSolution :: Name -> Int -> IO (Maybe Answer)
+readSolution name probNum = do
+  let path = printf "solutions/%s_%03d.json" name probNum
+  inp <- B.readFile path
+  return $ decode inp
+
+calcHappiness :: Name -> Int -> IO Happiness
+calcHappiness name probNum = do
+  let probMark = "problem " ++ show probNum
+  problem <- maybe (fail $ "parse error: " ++ probMark) pure =<< readProblem probNum
+  answer <- maybe (fail $ "parse error: " ++ probMark) pure =<<readSolution name probNum
+  let path = printf "solutions/%s_%03d.json" name probNum
+  putStrLn $ "calulating happiness: " ++ path
+  return $! happiness problem answer
