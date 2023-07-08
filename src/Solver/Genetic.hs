ff52b308049e21bf9545c92fcea429af39240409
oga <sato.yuta.20@gmail.com>
2023-07-08 22:15:33 +0900

    fix warnings.

--- src/Solver/Genetic.hs
+++ src/Solver/Genetic.hs
@@ -20,6 +20,7 @@ import Solver (SolverF)
 type Point = (Double,Double)
 
 infixr 0 $$
+($$) :: Show a => (a -> b) -> a -> b
 f $$ x = traceShow x (f x)
 
 
@@ -29,7 +30,6 @@ getCandidates problem = unsafePerformIO (getCandidatesIO problem)
 getCandidatesIO :: Problem -> IO (Either String [Point])
 getCandidatesIO problem@(Problem{stage_width=w, stage_height=h ,stage_bottom_left=(zw,zh), musicians=ms}) = do
   putStrLn $ "stage-width: "++show w++", stage_height: "++show h++", musicians: "++show nMusician
-  g <- newStdGen
   putStrLn $ "generating initial placement randomly"
   initials <- sequence $ take 2 $ repeat $ randomPlace problem (zip[0..nMusician-1](repeat undefined)) []
   print (initials!!0 == initials!!1)
@@ -65,7 +65,7 @@ getCandidatesIO problem@(Problem{stage_width=w, stage_height=h ,stage_bottom_lef
 
 
 splitBy :: Int -> [a] -> [[a]]
-splitBy n [] = []
+splitBy _ [] = []
 splitBy n xs =
   let (as,bs) = splitAt n xs in as : splitBy n bs
 
@@ -81,48 +81,49 @@ randomPlace (Problem{stage_width=w, stage_height=h ,stage_bottom_left=(zw,zh)}) 
   g <- newStdGen
   let randws = [fromIntegral(r+zw')| r<-randomRs (10,truncate(w-10)::Int) g, let zw'=truncate zw]
       randhs = [fromIntegral(r+zh')| r<-randomRs (10,truncate(h-10)::Int) g, let zh'=truncate zh]
-  return $ map snd $ sortBy(compare`on`fst) $ randomPlace' targets maxRetry initial randws randhs
+  return $ map snd $ sortBy(compare`on`fst) $ randomPlace' targets maxRetry initial (zip randws randhs)
   where
     maxRetry :: Int
     maxRetry = 20
 
-    randomPlace' :: [(Int,Point)] -> Int -> [(Int,Point)] -> [Double]-> [Double] -> [(Int,Point)]
-    randomPlace' []               _ ps _   _   = ps
-    randomPlace' ((k,current):ks) 0 ps kxs kys = randomPlace' ks maxRetry ((k,current):ps) kxs kys
-    randomPlace' ((k,current):ks) r ps (kx:kxs) (ky:kys) = do
+    randomPlace' :: [(Int,Point)] -> Int -> [(Int,Point)] -> [Point] -> [(Int,Point)]
+    randomPlace' []               _ ps _   = ps
+    randomPlace' ((k,current):ks) 0 ps kxy = randomPlace' ks maxRetry ((k,current):ps) kxy
+    randomPlace' _                _ _  []  = error "can't happen"
+    randomPlace' ((k,current):ks) r ps ((kx,ky):kxy) = do
 --      traceShow k (return ())
       if (or [ isMusicianConflict (kx,ky) (x,y) | (_,(x,y))<-ps])
-        then randomPlace' ((k,current):ks) (r-1) ps kxs kys --retry
-        else randomPlace' ks maxRetry ((k,(kx,ky)):ps) kxs kys
+        then randomPlace' ((k,current):ks) (r-1) ps kxy --retry
+        else randomPlace' ks maxRetry ((k,(kx,ky)):ps) kxy
 
 
 isMusicianConflict :: Point -> Point -> Bool
 isMusicianConflict (x1,y1) (x2,y2) =
-  (x1-x2)^2 + (y1-y2)^2 < 100
+  (x1-x2)^(2::Int) + (y1-y2)^(2::Int) < 100
 
 -- embed bs' area(wl,wh,hl,hh) to as
 crossOver :: (Double,Double) -> (Double,Double) -> [Point] -> [Point] -> IO (Maybe [Point])
 crossOver (wl,wh) (hl,hh) as' bs' =
-  return $ case crossOver' [] (zip[0..]as') bs' of
+  return $ case crossOver' [] (zip[0..] $ zip as' bs') of
              [] -> Nothing
              rs -> Just rs
   where
     musicians :: [Int]
     musicians = [i |(i,(x,y))<-zip[0..]bs', wl<=x,x<=wh, hl<=y,y<=hh]
 
-    crossOver' :: [Point] -> [(Int,Point)] -> [Point] -> [Point]
-    crossOver' rs [] _ = reverse rs
-    crossOver' rs ((i,a):as) (b:bs) =
+    crossOver' :: [Point] -> [(Int,(Point,Point))] -> [Point]
+    crossOver' rs []             = reverse rs
+    crossOver' rs ((i,(a,b)):xs) =
 --      traceShow i $
       case (i`elem`musicians, or(map (isMusicianConflict b) rs)) of
-        (False,_) -> crossOver' (a:rs) as bs
-        (_,False) -> crossOver' (b:rs) as bs
+        (False,_) -> crossOver' (a:rs) xs
+        (_,False) -> crossOver' (b:rs) xs
         _         -> []
 
 -- replace n musicians in as randomly
 mutation :: Problem-> Int -> [Point] -> IO (Maybe [Point])
 mutation problem@(Problem{musicians=ms}) n as = do
   g <- newStdGen
-  let (rest,target) = partition (\(i,a)->i`elem`targets) (zip[0..]as)
+  let (rest,target) = partition (\(i,_)->i`elem`targets) (zip[0..]as)
       targets = nub $ take n $ randomRs (0,length ms-1) g
   fmap Just $ randomPlace problem target rest
