6136bb82c38e776ecb650c0847e9d7e9b838bdf2
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-08 13:53:13 +0900

    apply happiness-strategy

--- mains/happiness.hs
+++ mains/happiness.hs
@@ -1,14 +1,23 @@
 
 import System.Environment (getArgs)
 
+import Happiness (HaStrategy (..))
 import Solutions
 
 main :: IO ()
 main = do
   args <- getArgs
-  (name, pnum) <- case args of
-    name : pnum : _ -> (,) name <$> readIO pnum
+  (name, pnum, stgy) <- case args of
+    name : pnum : stgy : _ -> (,,) name <$> readIO pnum <*> getStrategy stgy
+    name : pnum : _ -> (,,) name <$> readIO pnum <*> pure Naive
     _               -> fail "NAME and PROBLEM_ID required."
 
-  h <- calcHappiness name pnum
+  h <- calcHappiness name pnum stgy
   putStrLn $ name ++ " " ++ show pnum ++ ": " ++ show h
+
+getStrategy :: String -> IO HaStrategy
+getStrategy s =
+  maybe (fail $ "unknown happiness-strategy " ++ s) pure $ lookup s
+  [ ("naive", Naive)
+  , ("weighted-avg", WeightedAverage)
+  ]
