00cfb8a762f766edac397a68ed612f0f77a8cab3
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-08 23:28:33 +0900

    update happiness command to receive JSON filename

--- src/Solutions.hs
+++ src/Solutions.hs
@@ -19,19 +19,22 @@ saveAnswer (name, solver) probNum = do
   putStrLn $ "writing " ++ path --- ++ " : happiness: " ++ show (happiness problem ans)
   B.writeFile path $ encode ans
 
-readSolution :: Name -> Int -> IO (Maybe Answer)
-readSolution name probNum = do
-  let path = printf "solutions/%s_%03d.json" name probNum
+readSolutionFile :: FilePath -> IO (Maybe Answer)
+readSolutionFile path = do
   inp <- B.readFile path
   return $ decode inp
 
-calcHappiness :: Name -> Int -> HaStrategy -> IO Happiness
-calcHappiness name probNum strategy = do
+readSolution :: Name -> Int -> IO (Maybe Answer)
+readSolution name probNum = do
+  let path = printf "solutions/%s_%03d.json" name probNum
+  readSolutionFile path
+
+calcHappiness :: FilePath -> Int -> HaStrategy -> IO Happiness
+calcHappiness path probNum strategy = do
   let probMark = "problem " ++ show probNum
   problem <- maybe (fail $ "parse error: " ++ probMark) pure =<< readProblem probNum
-  answer <- maybe (fail $ "parse error: " ++ probMark) pure =<<readSolution name probNum
-  let path = printf "solutions/%s_%03d.json" name probNum
-      extra = mkExtra problem answer
+  answer <- maybe (fail $ "parse error: " ++ probMark) pure =<< readSolutionFile path
+  let extra = mkExtra problem answer
       icompat = int_compat_happiness extra
   putStrLn $ unwords ["calulating", show strategy, show icompat, "happiness:", path]
   return $! Happiness.applyStrategy strategy extra problem answer
