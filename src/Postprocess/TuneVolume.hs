652c0ecfe0c522d0c7559b1822f9ed353e496f36
Masahiro Sakai <masahiro.sakai@gmail.com>
2023-07-10 17:10:18 +0900

    add Postprocess.TuneVolume

---
+++ src/Postprocess/TuneVolume.hs
@@ -0,0 +1,27 @@
+module Postprocess.TuneVolume
+  ( tuneVolume
+  ) where
+
+import qualified Data.IntMap (IntMap)
+import qualified Data.IntMap.Strict as IntMap
+
+import Answer (Answer)
+import qualified Answer
+import Problem (Problem)
+import qualified Problem
+import Extra (Extra)
+import qualified Happiness
+
+
+tuneVolume :: Extra -> Problem -> Answer -> Answer
+tuneVolume extra prob ans
+  = ans
+  { Answer.volumes = Just $
+      [ if IntMap.findWithDefault 0 k table >= 0 then 10 else 0
+      | k <- [0..n-1]
+      ]
+  }
+  where
+    n = length (Problem.musicians prob)
+    ans0 = ans{ Answer.volumes = Nothing {- Just (replicate n 1.0) -} }
+    table = IntMap.fromListWith (+) [(k, val)  | (_i, k, val) <- Happiness.naiveUnreduced extra prob ans0]
