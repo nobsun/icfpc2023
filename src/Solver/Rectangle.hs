4880353f4278ba2f4937698220bdda73ebac4320
cutsea110 <cutsea110@gmail.com>
2023-07-10 11:32:20 +0900

    simple positions

--- src/Solver/Rectangle.hs
+++ src/Solver/Rectangle.hs
@@ -1,5 +1,8 @@
 module Solver.Rectangle where
 
+import Problem
+import Solver (SolverF)
+
 type Point = (Double, Double)
 
 -- | ステージの外周を 10 間隔でミュージシャンを配置すると
@@ -77,3 +80,46 @@ inRangeOfSouthWestMusician :: Point -> Point -> Bool
 inRangeOfSouthWestMusician (x0, y0) (x, y) =
   sqrt 3 * (x - x0) + (y - y0) <= 0 &&
   (x - x0) + sqrt 3 * (y - y0) <= 0
+
+stageBounds :: Problem -> (Double, Double, Double, Double)
+stageBounds prob = (left, top, right, bottom)
+  where
+    (left, bottom) =  (stage_left prob, stage_bottom prob)
+    (top, right) = (stage_top prob, stage_right prob)
+
+data Direction = West
+               | North
+               | East
+               | South
+               | NorthWest
+               | NorthEast
+               | SouthEast
+               | SouthWest
+               | Inner
+  deriving (Show, Eq)
+
+positions :: Problem -> [(Point, Direction)]
+positions prob = map withDirection poss
+  where
+    withDirection p@(x, y) = (p, direction p)
+    direction (x, y)
+      | x == westEdge && y == northEdge = NorthWest
+      | x == eastEdge && y == northEdge = NorthEast
+      | x == eastEdge && y == southEdge = SouthEast
+      | x == westEdge && y == southEdge = SouthWest
+      | x == westEdge = West
+      | x == eastEdge = East
+      | y == northEdge = North
+      | y == southEdge = South
+      | otherwise = Inner
+    
+    (w, n, e, s) = stageBounds prob
+    poss = [ (x, y)
+           | x <- [w+10.0, w+20.0 .. e-10.0]
+           , y <- [n-10.0, n-20.0 .. s+10.0]
+           , x <= e-10.0 && y >= s+10.0 -- Double なのでこれがないと誤差でステージに近すぎる場合が出る
+           ]
+    westEdge = minimum $ map fst poss
+    eastEdge = maximum $ map fst poss
+    northEdge = maximum $ map snd poss
+    southEdge = minimum $ map snd poss
