a138b8e17f3f938f8d442eae7831c70923069ed4
Masahiro Sakai <masahiro.sakai@gmail.com>
2023-07-10 11:30:16 +0900

    debug

--- src/Solver/NumericOptimization.hs
+++ src/Solver/NumericOptimization.hs
@@ -3,16 +3,19 @@ module Solver.NumericOptimization
   ) where
 
 import Control.Monad
+import Data.Array.IArray
 import Data.Array.Unboxed
 import Data.Either
 import qualified Data.Vector as V
 import qualified Data.Vector.Storable as VS
+import qualified Data.Vector.Generic as VG
 import Data.Vector.Storable (Vector)
 import System.IO.Unsafe
 import qualified System.Random.MWC as Rand
 import qualified System.Random.MWC.Distributions as Rand
 import Text.Printf
 import qualified Numeric.Optimization as Opt
+import Numeric.AD
 
 import Problem (Problem (..))
 import qualified Problem
@@ -44,13 +47,13 @@ getCandidatesIO problem = do
         k <- [0..numMusicians-1]
         return ((i,k), tasteTable ! (i, Problem.musicians problem !! k))
 
-      squareDistance :: (Vector Double, Vector Double) -> UArray (Int, Int) Double
+      squareDistance :: (VG.Vector v e, IArray a e, Fractional e) => (v e, v e) -> a (Int, Int) e
       squareDistance (xs, ys) = array ((0,0), (numAttendees-1, numMusicians-1)) $ do
         (i, Problem.Attendee{ Problem.x = x1, Problem.y = y1 }) <- zip [0..] (Problem.attendees problem)
         k <- [0..numMusicians-1]
-        let x2 = xs VS.! k
-            y2 = ys VS.! k
-        return ((i,k), (x1 - x2)^(2::Int) + (y1 - y2)^(2::Int))
+        let x2 = xs VG.! k
+            y2 = ys VG.! k
+        return ((i,k), (realToFrac x1 - x2)^(2::Int) + (realToFrac y1 - y2)^(2::Int))
 
       squareDistanceBackward :: (Vector Double, Vector Double) -> UArray (Int, Int) Double -> (Vector Double, Vector Double)
       squareDistanceBackward (xs, ys) gret =
@@ -66,31 +69,47 @@ getCandidatesIO problem = do
                     ]
         )
 
-      -- ブロックされているかは考慮しないバージョン
+      -- ブロックされているかは考慮しないバージョンのhappinessとその勾配を計算
       -- 数値が大きくなりすぎると計算が不安定になるかもと思って 1e6 をかけない状態での値で扱っている
       happyness :: (Vector Double, Vector Double) -> (Double, (Vector Double, Vector Double))
       happyness ps = (y, z)
         where
+          d :: UArray (Int,Int) Double
           d = squareDistance ps
           y = sum [(weightTable ! ik) / (d ! ik) | ik <- indices weightTable]
           z = squareDistanceBackward ps $ array (bounds weightTable) [(ik, (weightTable ! ik) * (-1) * (d ! ik)^^(-2::Int)) | ik <- indices weightTable]
 
+      -- デバッグ用
+      happynessAD :: (Vector Double, Vector Double) -> (Double, (Vector Double, Vector Double))
+      happynessAD (xs, ys) =
+        case grad' f (VG.convert (xs <> ys) :: V.Vector Double) of
+          (val, xs'_ys') -> (val, VG.splitAt numMusicians $ VG.convert xs'_ys')
+        where
+          f :: forall e. Fractional e => V.Vector e -> e
+          f xs_ys = y
+            where
+              ps = V.splitAt numMusicians xs_ys
+              d :: Array (Int,Int) e
+              d = squareDistance ps
+              y = sum [realToFrac (weightTable ! ik) / (d ! ik) | ik <- indices weightTable]
+
+      eps :: RealFrac e => e
       eps = 1e-6
 
-      penalty :: (Vector Double, Vector Double) -> Double
+      penalty :: (VG.Vector v e, RealFrac e) => (v e, v e) -> e
       penalty (xs, ys) = sum
         [ 1 / (d2 / 100 + eps)
         | (k1, k2) <- pairs [0..numMusicians-1]
-        , let (x1, y1) = (xs VS.! k1, ys VS.! k1)
-        , let (x2, y2) = (xs VS.! k2, ys VS.! k2)
+        , let (x1, y1) = (xs VG.! k1, ys VG.! k1)
+        , let (x2, y2) = (xs VG.! k2, ys VG.! k2)
         , let d2 = (x1 - x2)^(2::Int) + (y1 - y2)^(2::Int)
         , d2 < 100
         ]
 
       penaltyBackward :: (Vector Double, Vector Double) -> Double -> (Vector Double, Vector Double)
       penaltyBackward (xs, ys) gret = 
-        ( VS.replicate numMusicians 0 VS.// lefts us
-        , VS.replicate numMusicians 0 VS.// rights us
+        ( VS.accum (+) (VS.replicate numMusicians 0) (lefts us)
+        , VS.accum (+) (VS.replicate numMusicians 0) (rights us)
         )
         where
           us = concat
@@ -100,12 +119,19 @@ getCandidatesIO problem = do
               , Right (k2, a * 2 * (y1 - y2) * (-1))
               ]
             | (k1, k2) <- pairs [0..numMusicians-1]
-            , let (x1, y1) = (xs VS.! k1, ys VS.! k1)
-            , let (x2, y2) = (xs VS.! k2, ys VS.! k2)
+            , let (x1, y1) = (xs VG.! k1, ys VG.! k1)
+            , let (x2, y2) = (xs VG.! k2, ys VG.! k2)
             , let d2 = (x1 - x2)^(2::Int) + (y1 - y2)^(2::Int)
             , d2 < 100
             , let a = gret * (-1) * (d2 / 100 + eps)^^(-2::Int) * (1/100)
             ]
+
+      -- デバッグ用
+      penaltyBackwardAD :: (Vector Double, Vector Double) -> Double -> (Vector Double, Vector Double)
+      penaltyBackwardAD (xs, ys) gret
+        = VG.splitAt numMusicians $ VG.map (gret *) $ VG.convert
+        $ grad (\xs_ys -> penalty (VG.splitAt numMusicians xs_ys))
+        $ (VG.convert (xs <> ys) :: V.Vector Double)
 
       penaltyWeight :: Double
       penaltyWeight = 10
@@ -123,8 +149,6 @@ getCandidatesIO problem = do
 
       g :: Vector Double -> Vector Double
       g inp = VS.zipWith (+) (VS.map negate gxs) gxs2 <> VS.zipWith (+) (VS.map negate gys) gys2
-        -- VS.map negate (gxs <> gys)
-        -- gxs2 <> gys2
         where
           ps = VS.splitAt numMusicians inp
           (gxs, gys) = snd $ happyness ps
@@ -150,7 +174,11 @@ getCandidatesIO problem = do
   result <- Opt.minimize Opt.LBFGSB params (f `Opt.WithGrad` g `Opt.WithBounds` bounds) x0
 
   -- メッセージのスペースに注意
-  if Opt.resultSuccess result || Opt.resultMessage result `elem` ["The number of steps exceeded the user's request.", "ABNORMAL_TERMINATION_IN_LNSRCH                              "] then do
+  if Opt.resultSuccess result then do
+    let (xs,ys) = VS.splitAt numMusicians (Opt.resultSolution result)
+    return $ Right (zip (VS.toList xs) (VS.toList ys))
+  else if Opt.resultMessage result `elem` ["The number of steps exceeded the user's request.", "ABNORMAL_TERMINATION_IN_LNSRCH                              "] then do
+    traceM (Opt.resultMessage result)
     let (xs,ys) = VS.splitAt numMusicians (Opt.resultSolution result)
     return $ Right (zip (VS.toList xs) (VS.toList ys))
   else
