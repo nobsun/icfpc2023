6ea59761b9ff5c1c3ef70981d5b2ffb345b98394
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-10 14:37:00 +0900

    apply different names for parallel test-cases

--- test/HappinessSpec.hs
+++ test/HappinessSpec.hs
@@ -25,13 +25,13 @@ spec = describe "happiness" $ do
     Just (ans :: Answer) <- decode <$> BL.readFile "solutions/arraying_026.json"
     h <- happiness prob ans
     h `shouldBe` 55122290
-  it "compute correct happiness for submission-p22-2023-07-08T02_25_50.863957478Z.json" $ do
+  it "compute correct happiness for submission-p22 parallel" $ do
     Just prob <- readProblem 22
     Just (ans :: Answer) <- decode <$> BL.readFile "solutions/submission-p22-2023-07-08T02_25_50.863957478Z.json"
     extra <- mkExtra prob ans
     h <- withQueue extra prob ans
     h `shouldBe` 23370633
-  it "compute correct happiness for arraying_026.json" $ do
+  it "compute correct happiness for arraying_026 parallel" $ do
     Just prob <- readProblem 26
     Just (ans :: Answer) <- decode <$> BL.readFile "solutions/arraying_026.json"
     extra <- mkExtra prob ans
