a9c4e21b162a80888e74fb191e389b9a9854d118
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-10 18:45:51 +0900

    extend happiness command

--- mains/happiness_main.hs
+++ mains/happiness_main.hs
@@ -11,30 +11,41 @@ import Solutions
 main :: IO ()
 main = do
   args <- getArgs
-  (name, pnum, stgy) <- case args of
-    name : pnum : stgy : _ -> (,,) name <$> readIO pnum <*> getStrategy stgy
-    name : pnum : _ -> (,,) name <$> readIO pnum <*> pure Parallel
-    _               -> do
-      usage
-      putStrLn ""
-      fail "NAME and PROBLEM_ID required."
+  (name, may_pnum, stgy) <- case args of
+    name : pnum : stgy : _ -> (,,) name <$> (Just <$> readIO pnum) <*> getStrategy stgy
+    name : pnum : [] -> (,,) name <$> (Just <$> readIO pnum) <*> pure Parallel
+    name : []        -> pure (name, Nothing, Parallel)
+    _               ->  usageAndFail "FILENAME or SOLVER_NAME and PROBLEM_ID required."
 
   isFileName <- doesFileExist name
-  let path
-        | isFileName  =  name
-        | otherwise   =  printf "solutions/%s_%03d.json" name pnum
+  (path, pnum) <- case may_pnum of
+    Just pnum
+      | isFileName       -> pure (name, pnum)
+      | otherwise        -> pure (printf "solutions/%s_%03d.json" name pnum, pnum)
+    Nothing
+      | isFileName ->  case fromFilenameHeulistic name of
+          Just (_, pnum) -> pure (name, pnum)
+          Nothing        -> usageAndFail "Fail to find PROBLEM_ID with heuristics."
+    _                    -> usageAndFail "FILENAME or SOLVER_NAME and PROBLEM_ID required."
 
   h <- calcHappiness path pnum stgy
   putStrLn $ path ++ " " ++ show pnum ++ ": " ++ show h
 
+usageAndFail :: String -> IO a
+usageAndFail s = do
+  usage
+  putStrLn ""
+  fail s
+
 usage :: IO ()
 usage = do
   putStr $ unlines
-    [ "Usage: happiness FILENAME PROBLEM_ID [STRATEGY_NAME]"
+    [ "Usage: happiness FILENAME [PROBLEM_ID] [STRATEGY_NAME]"
     , ""
     , "       happiness SOLVER_NAME PROBLEM_ID [STRATEGY_NAME]"
     , ""
-    , "first check FILENAME, when not exist, try solusions/{SOLVER_NAME}_nnn.json"
+    , "first check FILENAME, when not exist, try solusions/{SOLVER_NAME}_nnn.json."
+    , "when PROBLEM_ID is not passed, calculating it with heuristics."
     , ""
     , "supported strategies:"
     , "  para - Parallel strategy (Naive with parallel execution)"
